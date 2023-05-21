c57c55f3f0c27b055a9c6c70f5242ad456cf09e0
190939_190863_190901 <190863@students.au.edu.pk>
2023-05-21 20:50:55 +0500

    Message

--- App.js
+++ App.js
@@ -62,6 +62,18 @@ app.get('/file/:username', (req, res) => {
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(router);
 
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+
 app.listen(port, () => {
   console.log(`Server Started : Port Number ${port}`);
 });
