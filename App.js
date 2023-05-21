c88577a6b8e999248e3387e5d3b77c06369860f0
190939_190863_190901 <190863@students.au.edu.pk>
2023-05-21 21:57:55 +0500

    build

--- App.js
+++ App.js
@@ -14,18 +14,6 @@ const file = require("./models/file.js");
 const upload = multer({ dest: 'uploads/' });
 const CircularJSON = require('circular-json');
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'build')));
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'),function (err) {
-          if(err) {
-              res.status(500).send(err)
-          }
-      });
-  })
-}
-
 // Handle file uploads
 app.post('/upload', upload.single('file'), (req, res) => {
   const { id } = req.body;
@@ -73,6 +61,19 @@ app.get('/file/:username', (req, res) => {
 // Serve static files
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(router);
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+
 
 app.listen(port, () => {
   console.log(`Server Started : Port Number ${port}`);
